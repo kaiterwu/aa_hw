a4a9e6dd81e5fcb2956987d3b29dbd17b53fa353
Kaiter Wu <wukaiter@gmail.com>
2023-01-26 21:21:54 -0500

    creating map class

--- w3d5/map_class.rb
+++ w3d5/map_class.rb
@@ -1,5 +1,29 @@
 class Map 
     def initialize
-        @storage = 
+        @storage = []
+    end 
+
+    def set(key,value)
+        if @storage.empty?
+             @storage << [key,value]
+        elsif @storage.any?{|pairs| key == pairs[0]}
+            
+            
+        else  
+            @storage << [key,value]
+        end
+            
+    end 
+
+    def get(key)
+        @storage.each{|pairs|return pairs[-1] if key == pairs[0]}
+    end 
+
+    def delete(key)
+        @storage.each{|pairs|@storage.delete(pairs) if pairs[0] == key }
+    end 
+
+    def show 
+        print @storage 
     end 
 end 
