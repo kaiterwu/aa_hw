382d854f016b34b4873e222459df8f807db31005
Kaiter Wu <wukaiter@gmail.com>
2023-02-19 18:59:13 -0500

    finishing auth hw

---
+++ W7/W7D1/auth_hw/app/models/user.rb
@@ -0,0 +1,52 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    attr_reader :password
+    
+    validates :username, presence: true 
+    validates :session_token, presence: true, uniqueness: true 
+    validates :password_digest, presence: { message: 'Password can\'t be blank' }
+    validates :password, length: { minimum: 6, allow_nil: true }
+    before_validation :ensure_session_token
+
+    def self.find_by_credentials(username,password)
+        user = User.find_by(username: username)
+        return nil if user.nil? 
+        user.is_password?(password) ? user : nil 
+    end 
+
+    def generate_unique_session_token
+        loop do
+            session_token = SecureRandom::urlsafe_base64(16)
+            return session_token unless User.exists?(session_token: session_token)
+          end
+    end 
+ 
+    def password=(password)
+        @password = password 
+        self.password_digest = BCrypt::Password.create(password)
+    end 
+
+    def is_password?(password)
+        BCrypt::Password.new(password_digest).is_password?(password)
+    end 
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end 
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+end 
