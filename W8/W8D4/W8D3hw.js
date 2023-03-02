ce13b53f64bded85b95ae42039c9e71de21ddb2f
Kaiter Wu <wukaiter@gmail.com>
2023-03-01 23:02:49 -0500

    finishing phase 2

--- W8/W8D4/W8D3hw.js
+++ W8/W8D4/W8D3hw.js
@@ -1,1 +1,33 @@
 // Technically hw for W8D3 but is being done for W8D4 
+
+function titleize(names,callback) {
+    let titleized = names.map(name => `Mx. ${name} Jingleheimer Schmidt`);
+    callback(titleized);
+};
+
+titleize(["Mary", "Brian", "Leo"], (names) => {
+    names.forEach(name => console.group(name));
+});
+
+function Elephant(name,height,tricks) {
+    this.name = name 
+    this.height = height 
+    this.tricks = tricks 
+}
+
+Elephant.prototype.trumpet = function () {
+    console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+};
+
+Elephant.prototype.grow = function () {
+    this.height = this.height + 12 
+}
+
+Elephant.prototype.addTrick = function(trick) { 
+    this.tricks.push(trick);
+}
+
+Elephant.prototype.play = function() {
+    trickIndex = Math.floor(Math.random() * this.tricks.length);
+  console.log(`${this.name} is ${this.tricks[trickIndex]}!`);
+}
