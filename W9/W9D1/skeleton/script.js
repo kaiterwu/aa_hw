28dccc78e33d8bd8409c496d359a24e5adc8ceae
Kaiter Wu <wukaiter@gmail.com>
2023-03-06 21:41:35 -0500

    debugging final phae

--- W9/W9D1/skeleton/script.js
+++ W9/W9D1/skeleton/script.js
@@ -19,6 +19,22 @@ document.addEventListener("DOMContentLoaded", () => {
   // adding SF places as list items
 
   // --- your code here!
+  const favoriteSubmit = (e) =>{
+    e.preventDefault();
+
+    const favInput = document.querySelector(".favorite-input");
+    const favorite = favInput.value; 
+    favInput.value = "";
+
+    const newLi = document.createElement('li');
+    newLi.textContent = favorite; 
+
+    const favorites = document.getElementById("sf-places");
+    favorites.appendChild(newLi);
+  };
+
+  const listsubmitButt = document.querySelector(".favorite-submit");
+  listsubmitButt.addEventListener("click",favoriteSubmit);
 
 
 
@@ -26,6 +42,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // --- your code here!
 
+  const showPhotos = (e) => {
+    const photoFormDiv = document.querySelector(".photo-form-container");
+    if (photoFormDiv.className === "photo-form-container "){
+      photoFormDiv.className = "photo-form-container hidden"
+    }else {
+      photoFormDiv.className = "photo-form-container";
+    }
+
+  };
+  const photoFormShowButt = document.querySelector(".photo-show-button");
+  photoFormShowButt.addEventListener("click",showPhotos);
+
+  const processPhotoSubmit = (e) => {
+    e.preventDefault();
+
+    const photoUrlInput = document.querySelector(".photo-url-input");
+    const photoUrl = photoUrlInput.value;
+    photoUrlInput.value = ""
+
+    const newImg = document.createElement("img")
+    newImg.src = photoUrl;
+
+    const newPhotoLi = document.createElement("li")
+    newPhotoLi.appendChild(newImg);
+
+    const dogPhotoList = document.querySelector(".dog-photos")
+    dogPhotoList.appendChild(newPhotoLi);
+  }
+
+  const submitPhotoButt = document.querySelector("photo-url-submit")
+  submitPhotoButt.addEventListener("click",processPhotoSubmit)
 
 
 });
